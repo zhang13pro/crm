13ea1e1542609bd6c53b66b6f24b8961903ab796
zhangshimao <1796021400@qq.com>
2019-07-30 15:22:49 +0800

    旅游maven项目

---
+++ travel/src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
@@ -0,0 +1,26 @@
+package cn.itcast.travel.web.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @Author: bobobo
+ * @Date: 2019/7/29 15:03
+ * @Version：1.0
+ */
+@WebServlet("/registUserServlet")
+public class RegistUserServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        
+    }
+}
